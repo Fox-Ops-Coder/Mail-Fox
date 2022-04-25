e9c752f641094133dcd85ab300489815aa7f9d6e
Лис Лесной <andrej.akimov.2014@outlook.com>
2022-04-25 17:16:01 +0300

    Добавил интерфейс для управления почтой

---
+++ Mail Fox/Mailing/Services/IMailService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace Mailing.Services
+{
+    public interface IMailService
+    {
+        Task AuthorizeAsync(string emailAddress);
+
+        Task SendMessageAsync(MailMessage message);
+
+        Task RemoveMessage(MailMessage message);
+
+        Task<IEnumerable<MailMessage>> GetMessagesAsync();
+    }
+}
