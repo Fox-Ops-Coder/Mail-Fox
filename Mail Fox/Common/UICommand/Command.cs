b34546f28401c92089943e458e0c3c62c7863524
Лис Лесной <andrej.akimov.2014@outlook.com>
2022-04-29 10:33:11 +0300

    Реализован класс команды

---
+++ Mail Fox/Common/UICommand/Command.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Common.UICommand
+{
+    public sealed class Command : ICommand
+    {
+        private readonly Action<object?> performAction;
+        private readonly Func<object?, bool>? isExecutable;
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public Command(Action<object?> performAction, Func<object?, bool>? isExecutable = null)
+        {
+            this.performAction = performAction;
+            this.isExecutable = isExecutable;
+        }
+
+        public bool CanExecute(object? param) => isExecutable == null || isExecutable(param);
+
+        public void Execute(object? param) => performAction(param);
+    }
+}
