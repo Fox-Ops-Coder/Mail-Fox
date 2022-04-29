2dd244c827c161d0ecf795955531c8818bfdf255
Лис Лесной <andrej.akimov.2014@outlook.com>
2022-04-29 15:08:10 +0300

    Добавил проект основного приложения

---
+++ Mail Fox/Mail Fox/App/MailFoxApp.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace MailFox.App
+{
+    /// <summary>
+    /// Interaction logic for MailFoxApp.xaml
+    /// </summary>
+    internal sealed partial class MailFoxApp : Application
+    {
+    }
+}
