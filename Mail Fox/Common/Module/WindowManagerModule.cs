40fa37ce3d8c72b67f76debbbd201a601ecfd5e6
Лис Лесной <andrej.akimov.2014@outlook.com>
2022-05-02 13:31:30 +0300

    Добавил внедрение зависимостей для менеджера окон

---
+++ Mail Fox/Common/Module/WindowManagerModule.cs
@@ -0,0 +1,11 @@
+﻿using Common.AppService.Manager;
+using Ninject.Modules;
+
+namespace Common.Module
+{
+    public sealed class WindowManagerModule : NinjectModule
+    {
+        public override void Load() => 
+            Bind<IWindowManager>().To<WindowManager>().InSingletonScope();
+    }
+}
