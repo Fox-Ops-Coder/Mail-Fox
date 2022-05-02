751f449ebe16af93493647daa17cdb8327fef28b
Лис Лесной <andrej.akimov.2014@outlook.com>
2022-05-02 14:15:57 +0300

    Добавил внедрение зависимостей для БД

---
+++ Mail Fox/MFData/Module/DatabaseModule.cs
@@ -0,0 +1,18 @@
+﻿using MFData.Context;
+using MFData.Core;
+using Ninject.Modules;
+
+namespace MFData.Module
+{
+    public sealed class DatabaseModule : NinjectModule
+    {
+        private readonly string dataSource;
+
+        public DatabaseModule(string dataSource) =>
+            this.dataSource = dataSource;
+
+        public override void Load() =>
+            Bind<IMFCore>().To<MFCore>()
+            .InSingletonScope().WithConstructorArgument("database", new MFDatabase(dataSource));
+    }
+}
