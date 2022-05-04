0ab5b5b07d82c695bf36f69c8ea1a3254b00a3a6
Лис Лесной <andrej.akimov.2014@outlook.com>
2022-05-04 08:40:40 +0300

    Добавил окно отправки сообщения и контекс данных

---
+++ Mail Fox/Mail Fox/UI/Mails/SendMailWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace MailFox.UI.Mails
+{
+    /// <summary>
+    /// Логика взаимодействия для SendMailWindow.xaml
+    /// </summary>
+    internal sealed partial class SendMailWindow : Window
+    {
+        public SendMailWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
